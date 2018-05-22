41ba5e48d7c811267cebae24ef995124cc21d0da
HomeSugar <zsugarhell@gmail.com>
2018-05-23 01:48:58 +0300

    PoolObject Test
    AudioSource PoolObject

---
+++ Assets/AudioSourcePool.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AudioSourcePool : MonoBehaviour
+{
+    PoolObject pool;
+    public GameObject Audiosource;
+    public AudioClip clip;
+
+    void Awake()
+    {
+        pool = new PoolObject(Audiosource, "Audiopool");
+    }
+
+    private void Update()
+    {
+        var v3 = Input.mousePosition;
+        v3.z = 10.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            var go = pool.RequestObject();
+            go.transform.position = v3;
+            AudioSource src = go.GetComponent<AudioSource>();
+            src.spatialBlend = 1.0f;
+            src.clip = clip;
+            src.Play();
+        }
+    }
+
+}
---
+++ Assets/Scripts/PoolObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolObject{
+
+    int ObjectCount;
+    public List<GameObject> Objects;
+    string PoolName;
+    GameObject prefab;
+
+    public PoolObject(GameObject Prefab,string Name, int Count = 10 ) 
+    {
+        ObjectCount = Count;
+        prefab = Prefab;
+        PoolName = Name;
+        Objects = new List<GameObject>();
+        CreatePool();
+    }
+
+    private void CreatePool()
+    {
+        for(int i = 0; i < ObjectCount; i++)
+        {
+            GameObject go = GameObject.Instantiate(prefab);
+            Objects.Add(go);
+            go.name = PoolName + i;
+            go.SetActive(false);
+        }
+    }
+
+    private GameObject FindAvailableObject()
+    {
+        for(int i =0; i< Objects.Count; i++)
+        {
+            if (!Objects[i].activeInHierarchy)
+            {
+                return Objects[i];
+            }
+            
+        }
+        return null;
+    }
+
+    public GameObject RequestObject()
+    {
+        Debug.Log("requesting object");
+        GameObject go = FindAvailableObject();
+        go.SetActive(true);
+        return go;
+    }
+
+
+
+
+}
